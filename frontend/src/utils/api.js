c2cdb5ce3e8525b503e5e1dcb7bb49d2ce5ce3d1
Praveenraj2206 <praveenrajs2005@gmail.com>
2026-06-29 12:36:43 +0530

    Connect frontend to Render backend

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,13 +1,13 @@
-import axios from 'axios'
+import axios from "axios";
 
 const api = axios.create({
-  baseURL: 'https://context-bot-g9ph.onrender.com/api'
-})
+  baseURL: "https://context-bot-g9ph.onrender.com/api",
+});
 
 api.interceptors.request.use((config) => {
-  const token = localStorage.getItem('token')
-  if (token) config.headers.Authorization = `Bearer ${token}`
-  return config
-})
+  const token = localStorage.getItem("token");
+  if (token) config.headers.Authorization = `Bearer ${token}`;
+  return config;
+});
 
-export default api
+export default api;
